0dc13004542145435202f84db8516efaa6c51f7f
Maksym Shulha Igovich <ipz221_shmi@student.ztu.edu.ua>
2024-02-21 21:26:32 +0200

    Created sum project

---
+++ Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 10;
+            Console.WriteLine($"Sum: {a + b}");
+        }
+    }
+}
